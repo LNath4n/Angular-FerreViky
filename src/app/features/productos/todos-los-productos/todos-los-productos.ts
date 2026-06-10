d99f359e0ecc198b1695cc8e10a12571da7d4650
Jaqui <jaqueline.maldo.munoz@gmail.com>
2026-06-09 22:50:30 -0600

    Agrega campo de búsqueda por marca y categoria

--- src/app/features/productos/todos-los-productos/todos-los-productos.ts
+++ src/app/features/productos/todos-los-productos/todos-los-productos.ts
@@ -4,15 +4,20 @@ import { Router } from '@angular/router';
 import { Producto } from '@core/models/producto';
 import { ProductosService } from '@core/services/productos';
 import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 //Esto de @Component le dice a angular que esto es un componente, y le dice cual es su html  cual su css y sus importaciones
 @Component({
   selector: 'app-todos-los-productos', //Por si queremos llamar este HTML en otro HTML seria <app-todos-los-productos/> Reutilizar codigo
-  imports: [CommonModule],
+imports: [CommonModule, FormsModule],
   templateUrl: './todos-los-productos.html',
   styleUrl: './todos-los-productos.css',
 })
+
 export class TodosLosProductos {
+
+  textoBusqueda: string = '';
 
   productos$!: Observable<Producto[]>;
   marcas$!: Observable<string[]>;
@@ -31,4 +36,16 @@ export class TodosLosProductos {
     this.router.navigate(['/productos', id]);
   }
 
+buscarPorMarca() {
+  const texto = this.textoBusqueda.toLowerCase().trim();
+
+  this.productos$ = this.productosService.getAll().pipe(
+    map(productos =>
+      productos.filter(producto =>
+        producto.marca.toLowerCase().includes(texto) ||
+        producto.categoria.toLowerCase().includes(texto)
+      )
+    )
+  );
+}
 }
